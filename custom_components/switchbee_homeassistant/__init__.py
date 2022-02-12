c23174c1eaa40af7ff282c91406bf355c277c73f
Bar Perach <barp12@gmail.com>
2022-02-12 15:17:01 +0200

    setup_entry should return true

--- custom_components/switchbee_homeassistant/__init__.py
+++ custom_components/switchbee_homeassistant/__init__.py
@@ -24,6 +24,7 @@ async def async_setup_entry(hass: core.HomeAssistant,
 
   hass.async_create_task(
       hass.config_entries.async_forward_entry_setup(entry, "switch"))
+  return True
 
 
 async def async_unload_entry(hass: core.HomeAssistant,
